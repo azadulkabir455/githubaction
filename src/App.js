7efc12f734388ea45103822350ec93309e14d677
azad ul kabir <azadulkabir10@gmail.com>
2024-05-14 20:29:35 +0600

    commit 6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { Link} from 'react-router-dom';
-import "./App.css";
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 
 function App() {
-  return (  
+  return (
     <>
-    <div className="nav">
-      <Link to="/">Home</Link>
-      <Link to="about">About</Link>
-      <Link to="contact">Contact</Link>
-    </div>
-
+      <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="contact">Contact</Link>
+      </div>
+      <Routes>
+        <Route path='/' Component={Home} />
+        <Route path='/about' Component={About} />
+        <Route path='/contact' Component={Contact} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,21 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from "react-router-dom";
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <App />
-      <Routes>
-        <Route path='/' Component={Home} />
-        <Route path='/about' Component={About} />
-        <Route path='/contact' Component={Contact} />
-      </Routes>
     </Router>
   </React.StrictMode>
 );
