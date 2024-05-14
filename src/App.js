c3b095b261eac370c27df6bbb47a704d85cd5e47
azad ul kabir <azadulkabir10@gmail.com>
2024-05-14 19:52:47 +0600

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './app.css';
 import { Link} from 'react-router-dom';
+import './app.css';
 
 function App() {
   return (  
     <>
     <div className="nav">
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="about">About</Link>
+      <Link to="contact">Contact</Link>
     </div>
 
     </>
